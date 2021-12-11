d170873ccbcd73874881be68b903a262f7083adf
ahmed-elmountassir <ahmedelmountassir10@gmail.com>
2021-12-11 09:43:50 +0100

    server

--- server.c
+++ server.c
@@ -6,22 +6,34 @@
 /*   By: ahel-mou <ahel-mou@student-1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/07 07:55:26 by ahel-mou          #+#    #+#             */
-/*   Updated: 2021/12/09 07:20:10 by ahel-mou         ###   ########.fr       */
+/*   Updated: 2021/12/10 00:45:45 by ahel-mou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "program.h"
 
-void sig_type(int num)
+void	combine(char bit)
 {
-	if (num == SIGUSR1)
+	static char	sms[8];
+	static int	i;
+
+	sms[i] = bit;
+	i++;
+	if (i == 8)
 	{
-		printf("0\n");
+		sms[i] = 0;
+		printf("str = %s", sms);
+		ft_printf("%c", rj3olia_ascii(sms));
+		i = 0;
 	}
-	else if (num == SIGUSR2)
-	{
-		printf("1\n");
-	}
+}
+
+void	sig_type(int sig)
+{
+	if (sig == SIGUSR1)
+		combine('0');
+	else if (sig == SIGUSR2)
+		combine('1');
 }
 
 int main()
