efaa9aebba57a830adad023fae60b636c8bbdf8b
ahmed-elmountassir <ahmedelmountassir10@gmail.com>
2021-12-12 11:54:07 +0100

    main server

--- server.c
+++ server.c
@@ -6,40 +6,47 @@
 /*   By: ahel-mou <ahel-mou@student-1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/07 07:55:26 by ahel-mou          #+#    #+#             */
-/*   Updated: 2021/12/10 00:45:45 by ahel-mou         ###   ########.fr       */
+/*   Updated: 2021/12/12 11:49:33 by ahel-mou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "program.h"
+#include "minitalk.h"
 
-void	combine(char bit)
+void	send_bit(char bit)
 {
-	static char	sms[8];
 	static int	i;
-
+	static char	sms[8];
+	
 	sms[i] = bit;
 	i++;
 	if (i == 8)
 	{
 		sms[i] = 0;
-		printf("str = %s", sms);
-		ft_printf("%c", rj3olia_ascii(sms));
+		ft_printf("%c", to_ascii(sms));
 		i = 0;
 	}
 }
 
-void	sig_type(int sig)
+void	sigtype(int sig, siginfo_t *data, void *pp)
 {
-	if (sig == SIGUSR1)
-		combine('0');
+	printf("\nclient pid= %d \n", data->si_pid);
+	if(sig == SIGUSR1)
+		send_bit('0');
 	else if (sig == SIGUSR2)
-		combine('1');
+		send_bit('1');
+	
 }
 
-int main()
+int	main()
 {
-	ft_printf("---%d---\n", getpid());
-	signal(SIGUSR1, sig_type);
-	signal(SIGUSR2, sig_type);
-	while (1);
+	ft_printf("----%d----\n", getpid());
+	struct sigaction sigs;
+    sigs.sa_sigaction = sigtype;
+    sigs.sa_flags = SA_SIGINFO;
+    sigaction(SIGUSR1, &sigs, NULL);
+	sigaction(SIGUSR2, &sigs, NULL);
+	while (1)
+	{
+		sleep(1);
+	}	
 }
