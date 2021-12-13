06d75a56b7b75baf1ef100be9a716661019765da
ahmed-elmountassir <ahmedelmountassir10@gmail.com>
2021-12-13 11:44:42 +0100

    server

--- server.c
+++ server.c
@@ -6,44 +6,71 @@
 /*   By: ahel-mou <ahel-mou@student-1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/07 07:55:26 by ahel-mou          #+#    #+#             */
-/*   Updated: 2021/12/12 11:49:33 by ahel-mou         ###   ########.fr       */
+/*   Updated: 2021/12/12 21:51:30 by ahel-mou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minitalk.h"
 
-void	send_bit(char bit)
+int		g_pid = 0;
+
+void	end_of_call(char bit, int pid)
+{
+	static int	zero;
+
+	if (bit == '0')
+	{
+		zero++;
+		if (zero == 8)
+			kill(pid, SIGUSR1);
+	}
+	else
+		zero = 0;
+}
+
+void	send_bit(char bit, int pid)
 {
 	static int	i;
-	static char	sms[8];
-	
-	sms[i] = bit;
-	i++;
-	if (i == 8)
+	static char	sms[9];
+
+	if (g_pid != pid)
 	{
-		sms[i] = 0;
-		ft_printf("%c", to_ascii(sms));
+		g_pid = pid;
 		i = 0;
+	}
+	sms[i] = bit;
+	if (g_pid == pid)
+	{
+		i++;
+		if (i == 9)
+		{
+			sms[i] = 0;
+			ft_printf("%c", to_ascii(sms));
+			i = 0;
+		}
+		end_of_call(bit, pid);
 	}
 }
 
 void	sigtype(int sig, siginfo_t *data, void *pp)
 {
-	printf("\nclient pid= %d \n", data->si_pid);
-	if(sig == SIGUSR1)
-		send_bit('0');
+	int	pid;
+
+	pid = data->si_pid;
+	if (sig == SIGUSR1)
+		send_bit('0', pid);
 	else if (sig == SIGUSR2)
-		send_bit('1');
-	
+		send_bit('1', pid);
 }
 
-int	main()
+int	main(void)
 {
+	struct sigaction	sigs;
+
 	ft_printf("----%d----\n", getpid());
-	struct sigaction sigs;
-    sigs.sa_sigaction = sigtype;
-    sigs.sa_flags = SA_SIGINFO;
-    sigaction(SIGUSR1, &sigs, NULL);
+	sigs.sa_sigaction = sigtype;
+	sigs.sa_flags = SA_SIGINFO;
+	sigaction(SIGUSR1, &sigs, NULL);
 	sigaction(SIGUSR2, &sigs, NULL);
 	while (1)
 	{
