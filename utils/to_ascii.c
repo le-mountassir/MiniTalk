70da0ab3ca2a422034cb0dac2c0e004bfd3dc874
ahmed-elmountassir <ahmedelmountassir10@gmail.com>
2021-12-13 11:47:29 +0100

    printf

--- utils/to_ascii.c
+++ utils/to_ascii.c
@@ -6,42 +6,27 @@
 /*   By: ahel-mou <ahel-mou@student-1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/11 13:26:03 by ahel-mou          #+#    #+#             */
-/*   Updated: 2021/12/11 18:40:55 by ahel-mou         ###   ########.fr       */
+/*   Updated: 2021/12/12 17:11:15 by ahel-mou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../minitalk.h"
-#include <math.h>
-static int	ft_pow(int num, int pow)
-{
-	int	i;
-	int	rt;
-
-	rt = num;
-	i = 0;
-	if (pow == 0)
-		return (1);
-	if (pow == 1)
-		return (num);
-	while (i < pow)
-	{
-		rt *= num;
-		i++;
-	}
-	return (i);
-}
 
 int	to_ascii(char *bin)
 {
-	int	ascii;
+	int	ng;
+	int	dec;
 	int	i;
 
-	ascii = 0;
-	i = 0;
-	while (i < 8)
+	ng = 1;
+	if (bin[0] == '1')
+		ng = -1;
+	dec = 0;
+	i = 1;
+	while (bin[i])
 	{
-		ascii += (bin[i] - 48) * pow(2, ((8 - i) - 1));
+		dec = dec << 1 | bin[i] - 48;
 		i++;
 	}
-	return (ascii);
+	return (dec * ng);
 }
--- utils/to_binary.c
+++ utils/to_binary.c
@@ -6,24 +6,36 @@
 /*   By: ahel-mou <ahel-mou@student-1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/11 18:15:20 by ahel-mou          #+#    #+#             */
-/*   Updated: 2021/12/11 18:24:21 by ahel-mou         ###   ########.fr       */
+/*   Updated: 2021/12/12 17:13:34 by ahel-mou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../minitalk.h"
 
-char	*to_binary(char ascii)
+char	*to_binary(char cara)
 {
-	char	*bin;
 	int		i;
+	char	*byte;
 
+	byte = malloc(sizeof(char) * 10);
+	if (!byte)
+	{
+		ft_printf("Allocation Failed !\n");
+		exit(1);
+	}
+	byte[9] = '\0';
+	byte[0] = '0';
+	if (cara < 0)
+	{
+		byte[0] = '1';
+		cara *= -1;
+	}
 	i = 8;
-	bin = malloc(8);
-    bin[8] = '\0';
-    while (--i >= 0)
-    {
-        bin[i] = ascii % 2 + 48;
-        ascii /= 2;
-    }
-	return (bin);
+	while (i >= 1)
+	{
+		byte[i] = cara % 2 + '0';
+		cara /= 2;
+		i--;
+	}
+	return (byte);
 }
