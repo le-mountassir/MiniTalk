7738ab5e57c86586050438aa23ea28766ad3c433
ahmed-elmountassir <ahmedelmountassir10@gmail.com>
2021-12-13 11:44:32 +0100

    client

--- client.c
+++ client.c
@@ -6,7 +6,7 @@
 /*   By: ahel-mou <ahel-mou@student-1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/07 07:55:23 by ahel-mou          #+#    #+#             */
-/*   Updated: 2021/12/12 11:43:01 by ahel-mou         ###   ########.fr       */
+/*   Updated: 2021/12/13 01:34:24 by ahel-mou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,46 +14,74 @@
 
 void	send_it(char *sms, int pid)
 {
-	int i;
-	
+	int	i;
+
 	i = 0;
-	printf("\nim sending : ");
 	while (sms[i])
 	{
-		printf("%c", sms[i]);
 		if (sms[i] == '0')
 			kill(pid, SIGUSR1);
-		else if(sms[i] == '1')
+		else if (sms[i] == '1')
 			kill(pid, SIGUSR2);
 		i++;
 		usleep(500);
 	}
+	free(sms);
 }
 
-int main(int c, char **v)
+void	confirm(int sig)
+{
+	sig = 0;
+	ft_printf("im done i sent everything\n");
+}
+
+void	sms_to_bin(t_storage_unit *unit_2)
+{
+	int	i;
+	int	len;
+	int	f;
+
+	len = ft_strlen(unit_2->str);
+	i = 0;
+	f = 0;
+	while (f < len + 1)
+	{
+		unit_2->rt_bin = to_binary(unit_2->str[f]);
+		unit_2->rt_bin[9] = '\0';
+		while (*unit_2->rt_bin)
+		{
+			unit_2->sms[i] = *unit_2->rt_bin;
+			i++;
+			unit_2->rt_bin++;
+		}
+		f++;
+	}
+	unit_2->sms[i] = '\0';
+}
+
+int	main(int c, char **v)
 {
 	t_storage_unit	unit_1;
-	printf("\n---%d---\n", getpid());
+
+	ft_printf("---%d---\n", getpid());
 	if (c == 3)
 	{
+		signal(SIGUSR1, confirm);
 		unit_1.pid = ft_atoi(v[1]);
+		if (unit_1.pid == 0)
+		{
+			ft_printf("wrong pid!!!\n");
+			return (0);
+		}
 		unit_1.str = v[2];
 		unit_1.sms = malloc(ft_strlen(unit_1.str) * 8);
-		unit_1.rt_bin = malloc(8);
-		unit_1.i = 0;
-		while (*unit_1.str)
-		{
-			unit_1.rt_bin = to_binary(*unit_1.str);
-			while (*unit_1.rt_bin)
-			{
-				unit_1.sms[unit_1.i] = *unit_1.rt_bin;
-				unit_1.i++;
-				unit_1.rt_bin++;
-			}
-			unit_1.str++;
-		}
-		unit_1.sms[unit_1.i] = '\0';
+		if (!unit_1.sms)
+			return (0);
+		unit_1.rt_bin = malloc(9);
+		if (!unit_1.rt_bin)
+			return (0);
+		sms_to_bin(&unit_1);
 		send_it(unit_1.sms, unit_1.pid);
+		free(unit_1.rt_bin);
 	}
 }
-
