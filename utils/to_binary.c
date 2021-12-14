c14cbad770e0ef7ef60444aac624a3d47146303c
ahmed-elmountassir <ahmedelmountassir10@gmail.com>
2021-12-14 10:58:15 +0100

    convert chars to binay

--- utils/to_binary.c
+++ utils/to_binary.c
@@ -6,7 +6,7 @@
 /*   By: ahel-mou <ahel-mou@student-1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/11 18:15:20 by ahel-mou          #+#    #+#             */
-/*   Updated: 2021/12/12 17:13:34 by ahel-mou         ###   ########.fr       */
+/*   Updated: 2021/12/14 01:50:45 by ahel-mou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,12 +17,9 @@ char	*to_binary(char cara)
 	int		i;
 	char	*byte;
 
-	byte = malloc(sizeof(char) * 10);
+	byte = malloc(10);
 	if (!byte)
-	{
-		ft_printf("Allocation Failed !\n");
 		exit(1);
-	}
 	byte[9] = '\0';
 	byte[0] = '0';
 	if (cara < 0)
